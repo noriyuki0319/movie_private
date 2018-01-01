2386c227d4b36c1b1672c732d2d5d3b0bc955c5e
noriyuki <you.know.it.is.all.good.39@gmail.com>
2018-01-01 16:11:07 +0900

    done

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,76 +3,8 @@ class CategoriesController < ApplicationController
   def index
   end
 
-  def action
-    @action = Movie.joins(:reviews).where(category_id: "アクション／アドベンチャー").group("track_id")
-  end
-
-  def drama
-    @drama = Movie.joins(:reviews).where(category_id: "ドラマ").group("track_id")
-  end
-
-  def fantasy
-    @fantasy = Movie.joins(:reviews).where(category_id: "SF／ファンタジー").group("track_id")
-  end
-
-  def comedy
-    @comedy = Movie.joins(:reviews).where(category_id: "コメディ").group("track_id")
-  end
-
-  def romance
-    @romance = Movie.joins(:reviews).where(category_id: "ロマンス").group("track_id")
-  end
-
-  def kids
-    @kids = Movie.joins(:reviews).where(category_id: "キッズ／ファミリー").group("track_id")
-  end
-
-  def sports
-    @sports = Movie.joins(:reviews).where(category_id: "スポーツ").group("track_id")
-  end
-
-  def musical
-    @musical = Movie.joins(:reviews).where(category_id: "ミュージカル").group("track_id")
-  end
-
-  def anime
-    @anime = Movie.joins(:reviews).where(category_id: "アニメ").group("track_id")
-  end
-
-  def thriller
-    @thriller = Movie.joins(:reviews).where(category_id: "スリラー").group("track_id")
-  end
-
-  def documentary
-    @documentary = Movie.joins(:reviews).where(category_id: "ドキュメンタリー").group("track_id")
-  end
-
-  def masterpiece
-    @masterpiece = Movie.joins(:reviews).where(category_id: "名作").group("track_id")
-  end
-
-  def music_dc
-    @music_dc = Movie.joins(:reviews).where(category_id: "音楽ドキュメンタリー").group("track_id")
-  end
-
-  def music_mv
-    @music_mv = Movie.joins(:reviews).where(category_id: "音楽映画").group("track_id")
-  end
-
-  def horror
-    @horror = Movie.joins(:reviews).where(category_id: "ホラー").group("track_id")
-  end
-
-  def hobby
-    @hobby = Movie.joins(:reviews).where(category_id: "趣味／教養").group("track_id")
-  end
-
-  def japanese
-    @japanese = Movie.joins(:reviews).where(category_id: "日本映画").group("track_id")
-  end
-
-  def live
-    @live = Movie.joins(:reviews).where(category_id: "ライブビデオ").group("track_id")
+  def categories_list
+    @category = Movie.joins(:reviews).where(category_id: params[:keyword]).group("track_id")
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,23 +17,6 @@ Rails.application.routes.draw do
   root 'movies#index'
 
   get "categories/index", as: "categories"
-  get "categories/action", as: "action"
-  get "categories/drama", as: "drama"
-  get "categories/fantasy", as: "fantasy"
-  get "categories/comedy", as: "comedy"
-  get "categories/romance", as: "romance"
-  get "categories/kids", as: "kids"
-  get "categories/sports", as: "sports"
-  get "categories/musical", as: "musical"
-  get "categories/anime", as: "anime"
-  get "categories/thriller", as: "thriller"
-  get "categories/documentary", as: "documentary"
-  get "categories/masterpiece", as: "masterpiece"
-  get "categories/music_dc", as: "music_dc"
-  get "categories/music_mv", as: "music_mv"
-  get "categories/horror", as: "horror"
-  get "categories/hobby", as: "hobby"
-  get "categories/japanese", as: "japanese"
-  get "categories/live", as: "live"
+  get "categories/categories_list", as: "categories_list"
 
 end
